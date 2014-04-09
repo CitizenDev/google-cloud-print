4ebb7fa639529dca0264590b656b7bbfae82ee64
Eugene OZ <normandiggs@gmail.com>
2014-04-09 20:30:43 +0400

    v0.0.4

--- Jamm/GoogleCloudPrint/Printer.php
+++ Jamm/GoogleCloudPrint/Printer.php
@@ -6,7 +6,7 @@ class Printer
   /** @var API */
   private $API;
   private $id;
-  private $settings;
+  private $ticket;
 
   public function __construct(API $API, $id)
   {
@@ -19,7 +19,7 @@ class Printer
     if (empty($job_title)) {
       $job_title = basename($file).' '.date('Y-m-d H:i:s');
     }
-    return $this->API->sendFileToPrinter($this->id, $job_title, $file, $content_type, $this->settings);
+    return $this->API->sendFileToPrinter($this->id, $job_title, $file, $content_type, $this->ticket);
   }
 
   public function printURL($url, $job_title = '')
@@ -27,7 +27,7 @@ class Printer
     if (empty($job_title)) {
       $job_title = basename($url).' '.date('Y-m-d H:i:s');
     }
-    return $this->API->sendURLToPrinter($this->id, $job_title, $url, $this->settings);
+    return $this->API->sendURLToPrinter($this->id, $job_title, $url, $this->ticket);
   }
 
   public function printContent($content, $content_type = 'application/pdf', $job_title = '')
@@ -35,22 +35,22 @@ class Printer
     if (empty($job_title)) {
       $job_title = $content_type.' '.date('Y-m-d H:i:s');
     }
-    return $this->API->sendContentToPrinter($this->id, $job_title, $content, $content_type, $this->settings);
+    return $this->API->sendContentToPrinter($this->id, $job_title, $content, $content_type, $this->ticket);
   }
 
   /**
    * @return mixed
    */
-  public function getSettings()
+  public function getTicket()
   {
-    return $this->settings;
+    return $this->ticket;
   }
 
   /**
    * @param mixed $settings
    */
-  public function setSettings($settings)
+  public function setTicket($settings)
   {
-    $this->settings = $settings;
+    $this->ticket = $settings;
   }
 } 
